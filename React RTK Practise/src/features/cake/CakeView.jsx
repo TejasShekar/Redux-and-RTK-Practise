8ce1920dbe8974f1995f34099e80b512c5c8254e
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-04 00:11:16 +0530

    get respective state data

--- React RTK Practise/src/features/cake/CakeView.jsx
+++ React RTK Practise/src/features/cake/CakeView.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import {useSelector} from "react-redux";
 
 export const CakeView = () => {
+  const noOfCakes = useSelector((state) => state.cake.noOfCakes);
   return (
     <div>
-      <h2>Number of Cakes - </h2>
+      <h2>Number of Cakes - {noOfCakes}</h2>
       <button>Order Cake</button>
       <button>Restock Cake</button>
     </div>
--- React RTK Practise/src/features/icecream/IceCreamView.jsx
+++ React RTK Practise/src/features/icecream/IceCreamView.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import {useSelector} from "react-redux";
 
 export const IceCreamView = () => {
+  const noOfIceCreams = useSelector((state) => state.iceCream.noOfIcecreams);
   return (
     <div>
-      <h2>Number of Ice Creams - </h2>
+      <h2>Number of Ice Creams - {noOfIceCreams}</h2>
       <button>Order Ice Cream</button>
       <button>Restock Ice Cream</button>
     </div>
