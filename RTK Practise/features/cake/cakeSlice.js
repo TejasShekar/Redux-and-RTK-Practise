4c6845c3537ef1a88907a4d6480e6de77e872f57
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-03 13:22:48 +0530

    Update cakeSlice.js

--- RTK Practise/features/cake/cakeSlice.js
+++ RTK Practise/features/cake/cakeSlice.js
@@ -5,7 +5,7 @@ const initialState = {
   noOfCakes: 10,
 };
 
-// create a slice for cake. This slice is part of the store that is handled by redux.
+// create a slice for cake. This slice is part of the store that is handled by redux and managed individually.
 // You only need to create slice for specific state handling and redux toolkit takes cares of actions and action creators, the reducer fn and its switch statements.
 const cakeSlice = createSlice({
   name: "cake",
