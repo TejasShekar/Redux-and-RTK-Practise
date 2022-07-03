75238d76b28622b7c6da2834a2b42722b9d22adc
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-03 20:37:06 +0530

    Update index.js

--- RTK Practise/index.js
+++ RTK Practise/index.js
@@ -1,16 +1,17 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
-const iceCreamActions =
-  require("./features/icecream/iceCreamSlice").iceCreamActions;
+const iceCreamActions = require("./features/icecream/iceCreamSlice").iceCreamActions;
 
 console.log("Initial State", store.getState());
-const unsubscribe = store.subscribe(() => {});
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
 
-store.dispatch(cakeActions.ordered(3));
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(iceCreamActions.ordered(3));
-store.dispatch(iceCreamActions.ordered(3));
-store.dispatch(iceCreamActions.ordered(3));
-store.dispatch(iceCreamActions.restocked(9));
+// store.dispatch(cakeActions.ordered(3));
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(iceCreamActions.ordered(3));
+// store.dispatch(iceCreamActions.ordered(3));
+// store.dispatch(iceCreamActions.ordered(3));
+// store.dispatch(iceCreamActions.restocked(9));
 
 unsubscribe();
