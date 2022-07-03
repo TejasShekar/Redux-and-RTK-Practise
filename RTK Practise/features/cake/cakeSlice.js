3cf6ec6f347fa4bd2bb4d4e899de34f13c29f3cd
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-03 13:20:04 +0530

    Create cakeSlice.js

---
+++ RTK Practise/features/cake/cakeSlice.js
@@ -0,0 +1,24 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+// create initial state
+const initialState = {
+  noOfCakes: 10,
+};
+
+// create a slice for cake. This slice is part of the store that is handled by redux.
+// You only need to create slice for specific state handling and redux toolkit takes cares of actions and action creators, the reducer fn and its switch statements.
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState,
+  reducers: {
+    ordered: (state, action) => {
+      state.noOfCakes -= action.payload;
+    },
+    restocked: (state, action) => {
+      state.noOfCakes += action.payload;
+    },
+  },
+});
+
+module.exports = cakeSlice.reducer;
+module.exports.cakeActions = cakeSlice.actions;
