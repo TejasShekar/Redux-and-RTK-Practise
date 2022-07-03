a6d037f5d5f13769400c7072dc4803a3463fc21b
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-03 20:13:13 +0530

    add redux-logger as middleware

--- RTK Practise/app/store.js
+++ RTK Practise/app/store.js
@@ -2,11 +2,16 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreamReducer = require("../features/icecream/iceCreamSlice");
 
+const reduxLogger = require("redux-logger").createLogger();
+
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
   },
+  // configureStore applies a few middleware by default. So, we get all those middleware via getDefaultMiddleware and add our middlewares to it using concat().
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(reduxLogger),
 });
 
 module.exports = store;
